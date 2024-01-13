83b5db5fce240f00d1ac1afae40f81a8592b536a
Janne Valkealahti <janne.valkealahti@gmail.com>
2024-01-13 15:10:50 +0000

    init

---
+++ src/main/java/com/github/jvalkeal/TermdeckApplication.java
@@ -0,0 +1,13 @@
+package com.github.jvalkeal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TermdeckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TermdeckApplication.class, args);
+	}
+
+}
